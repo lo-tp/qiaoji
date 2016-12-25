4036779697960779f0a4ee430be2f9723529baec
lo-tp <regesteraccount@hotmail.com>
2016-12-25 22:09:18 +0800

    change linting settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,49 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: ['airbnb'],
   globals:{
+      DOMParser:true,
+      cordova:true,
+      window:true,
       document:true,
+      Connection:true,
+      navigator:true,
+      localStorage:true,
     },
+  parser: 'babel-eslint',
+  plugins: [
+    'babel',
+  ],
   rules:{
-      camelcase:0,
-      'react/forbid-prop-types':0,
-    },
+    'arrow-parens':[
+      2,
+      'as-needed',
+    ],
+    'react/jsx-curly-spacing': [
+      2,
+      'always',
+    ],
+    'jsx-quotes': [
+      2,
+      'prefer-single',
+    ],
+    'react/jsx-equals-spacing': [
+      2,
+      'always',
+    ],
+    'comma-dangle':[
+      2,
+      'always-multiline',
+    ],
+    'react/jsx-boolean-value': [
+      2,
+      'always',
+    ],
+    'no-underscore-dangle': [2, { allow: ['__', '_id', ] }],
+    'no-mixed-operators': [0],
+
+    'consistent-return':0,
+
+    camelcase:0,
+    'react/forbid-prop-types':0,
+  },
 };
