e6e1fd21996827e039dda1f6a72cb26185aca717
lo-tp <regesteraccount@hotmail.com>
2016-12-26 13:46:57 +0800

    Remove useless component in loginSignupReset page

--- client/account/loginSignupReset/component.jsx
+++ client/account/loginSignupReset/component.jsx
@@ -3,19 +3,6 @@ import { Tabs, Tab } from 'material-ui/Tabs';
 import SignupForm from './signupForm';
 import LoginForm from './loginForm/component';
 
-const styles = {
-  headline: {
-    fontSize: 24,
-    paddingTop: 16,
-    marginBottom: 12,
-    fontWeight: 400,
-  },
-};
-
-function handleActive(tab) {
-  alert(`A tab with this route property ${tab.props['data-route']} was activated.`);
-}
-
 const TabsExampleSimple = () => (
   <Tabs>
     <Tab label = 'Signup' >
@@ -23,18 +10,6 @@ const TabsExampleSimple = () => (
     </Tab>
     <Tab label = 'Login' >
       <LoginForm />
-    </Tab>
-    <Tab
-      label = 'onActive'
-      data-route = '/home'
-      onActive = { handleActive }
-    >
-      <div>
-        <h2 style = { styles.headline }>Tab Three</h2>
-        <p>
-          This is a third example tab.
-        </p>
-      </div>
     </Tab>
   </Tabs>
 );
