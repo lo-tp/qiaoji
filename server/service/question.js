1cdf3e28720ca08193212b37995452987f31674e
lo-tp <regesteraccount@hotmail.com>
2017-01-11 11:19:15 +0800

    write backend end code to handler question updation in question/update(post)

--- server/service/question.js
+++ server/service/question.js
@@ -13,6 +13,7 @@ router.get('/list', async (req, res) => {
       user: req.user._id,
     })
     .where('difficulty').gte(QUALIFY_DIFFICULTY)
+
   // eslint-disable-next-line newline-per-chained-call
     .where('dueDate').lte(getDaysSinceEpoch())
     .populate('answer')
@@ -60,6 +61,52 @@ router.get('/list', async (req, res) => {
   }
 
   res.end();
+});
+router.post('/update', async (req, res) => {
+  try {
+    const { questions } = req.body;
+    const ids = questions.map(q => q.id);
+
+    // validate if all the questions belong to the current user
+    const qs = await Question.find({ user: req.user._id })
+    .where('_id').in(ids);
+    if (qs.length < questions.length) {
+      res.status(500);
+    } else {
+      questions.forEach(q => {
+        const dataInDb = qs.find(w =>
+          // eslint-disable-next-line eqeqeq
+          w._id == `${q.id}`);
+        if (q.difficulty) {
+          dataInDb.difficulty = q.difficulty;
+        }
+
+        if (q.update) {
+          dataInDb.update = q.update;
+        }
+
+        if (q.dueDate) {
+          dataInDb.dueDate = q.dueDate;
+        }
+
+        if (q.interval) {
+          dataInDb.interval = q.interval;
+        }
+      });
+      const number = qs.length;
+      for (let index = 0; index < number; index += 1) {
+        await qs[index].save();
+      }
+
+      res.status(200);
+    }
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.info(e.message);
+    res.status(500);
+  } finally {
+    res.end();
+  }
 });
 
 export default router;
--- server/service/question.test.js
+++ server/service/question.test.js
@@ -8,6 +8,7 @@ import chai from 'chai';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import chaiHttp from 'chai-http';
 import app from '../server';
+import Question from '../model/question';
 import { dbSetupTest, dbClose } from '../setup/db';
 import { mockData, createUerAndCookie } from '../testTools';
 import { getDaysSinceEpoch } from '../../common/tool';
@@ -159,5 +160,124 @@ describe('get questions for a specific user', () => {
   after(done => {
     dbClose();
     done();
+  });
+});
+describe('update questions', () => {
+  const data = [];
+  const expected = [];
+  let results;
+  before(async () => {
+    dbSetupTest();
+    const tem = await createUerAndCookie();
+    user = tem.user;
+    cookie = tem.cookie;
+
+    // questions below are allowed to be updated
+    for (let index = 0; index < 3; index += 1) {
+      expected.push({
+        quiz: {
+          content: `content ${index}`,
+          title: `title ${index}`,
+        },
+        answer: {
+          content: `content ${index}`,
+        },
+        dueDate: getDaysSinceEpoch(),
+        update: 0,
+        interval: 0,
+        difficulty: QUALIFY_DIFFICULTY + 0.1,
+      });
+      data.push({
+        quiz: {
+          content: `content ${index}`,
+          title: `title ${index}`,
+          user: user._id,
+        },
+        answer: {
+          content: `content ${index}`,
+          user: user._id,
+        },
+        question: {
+          dueDate: getDaysSinceEpoch(),
+          update: 0,
+          interval: 0,
+          difficulty: QUALIFY_DIFFICULTY + 0.1,
+          user: user._id,
+        },
+      });
+    }
+
+    // these questions are not allowed to be updated
+    data.push(
+      {
+        quiz: {
+          content: 'content test',
+          title: 'title test',
+
+          // this is the reason for the exclusion
+          user: 'sdjf',
+        },
+        answer: {
+          content: 'content test',
+          user: 'sdjf',
+        },
+        question: {
+          dueDate: getDaysSinceEpoch(),
+          update: 0,
+          interval: 0,
+          difficulty: QUALIFY_DIFFICULTY + 0.1,
+          user: 'sdjf',
+        },
+      });
+    results = await mockData(data);
+  });
+  it('500 when update questions not belonging to the user', async() => {
+    await assert.equal(1, 1);
+    const updateTargets = results.map(r => {
+      const ret = {};
+      ret.id = `${r.question._id}`;
+      ret.dueDate = 12346789;
+      return ret;
+    });
+    try {
+      await chai.request(app)
+      .post(`${path}/update`)
+      .set('cookieId', cookie._id)
+      .send({
+        questions: updateTargets,
+      });
+      assert.equal(1, 500);
+    } catch (e) {
+      assert.equal(e.status, 500);
+    }
+  });
+  it('status 200 when every is ok', async() => {
+    assert.equal(1, 1);
+    const updateTargets = results.filter(r => r.question.user === `${user._id}`)
+      .map(r => {
+        const ret = {};
+        ret.id = `${r.question._id}`;
+        ret.dueDate = 12346789;
+        ret.difficulty = 0.8;
+        ret.interval = 165;
+        ret.update = 399;
+        return ret;
+      });
+    const res = await chai.request(app)
+    .post(`${path}/update`)
+    .set('cookieId', cookie._id)
+      .send({
+        questions: updateTargets,
+      });
+    assert.equal(res.status, 200);
+    const ids = updateTargets.map(q => q.id);
+    const questions = await Question.find({ user: user._id })
+    .where('_id').in(ids);
+    questions.forEach(q => {
+      assert.equal(q.dueDate, 12346789);
+      assert.equal(q.difficulty, 0.8);
+      assert.equal(q.interval, 165);
+      assert.equal(q.update, 399);
+    });
   });
 });
